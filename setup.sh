6207db4150a19fa260e571cef5973ad24ee19393
KOUAKOU Ulrich Gerard Emmanuel <emmanuelug.kouakou@gmail.com>
2021-08-02 16:44:23 +0200

    derniere modification

--- setup.sh
+++ setup.sh
@@ -1,12 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-primaryColor = '#E694FF'\n\
-backgroundColor = '#00172B'\n\
-secondaryBackgroundColor = '#0083B8'\n\
-textColor = '#DCDCDC'\n\
-font = 'sans-serif'\n\
-\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
