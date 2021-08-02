5d5bf55c2998708e7cb4704f03402a13beca590b
KOUAKOU Ulrich Gerard Emmanuel <emmanuelug.kouakou@gmail.com>
2021-08-02 16:27:41 +0200

    fichier remodified

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+primaryColor = '#E694FF'
+backgroundColor = '#00172B'
+secondaryBackgroundColor = '#0083B8'
+textColor = '#DCDCDC'
+font = 'sans-serif'
 [server]\n\
 headless = true\n\
 port = $PORT\n\
