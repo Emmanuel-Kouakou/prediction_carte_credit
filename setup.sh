a8a5670ca954312aa481135abc460ad763f83303
KOUAKOU Ulrich Gerard Emmanuel <emmanuelug.kouakou@gmail.com>
2021-08-02 17:20:53 +0200

    changement

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[theme]\n\
+primaryColor = '#E694FF'\n\
+backgroundColor = '#00172B'\n\
+secondaryBackgroundColor = '#0083B8'\n\
+textColor = '#DCDCDC'\n\
+font = 'sans serif'\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS=false\n\
 \n\
 " > ~/.streamlit/config.toml
+
